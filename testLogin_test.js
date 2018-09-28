c8760ca8e2d7e8fbcfa80620ef8cdb158252b4de
Mauricio Schwartzhaupt Narcizo <mauricionarcizo@gmail.com>
2018-09-28 08:08:04 -0300

    first commit

---
+++ testLogin_test.js
@@ -0,0 +1,12 @@
+Feature('My First Test');
+
+Scenario('test something', (I) => {
+  I.amOnPage('https://github.com');
+  I.see('GitHubss');
+  I.click('Sign in', '//html/body/div[1]/header');
+  I.see('Sign in to GitHub', 'h1');
+  I.fillField('Username or email address', 'something@totest.com');
+  I.fillField('Password', '123456');
+  I.click('Sign in');
+  I.see('Incorrect username or password.', '.flash-error');
+});
